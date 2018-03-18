9a48786ffa836bd766c3b577c149bb2bc5980a42
nuswee43 <manassawee.jar@mail.kmutt.ac.th>
2018-03-19 02:05:36 +0700

    [TestUpload]testupfile.js

---
+++ src/testupfile.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+describe('Temperature Conversion', function() {
+  describe('cToF', function() {
+    // tests here
+  });
+  describe('fToC', function() {
+    // tests here
+  });
+});
