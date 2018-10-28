fb48736472029a2474ccb95a1f666b318bea5f11
minnyww <minrrverse@gmail.com>
2018-10-28 23:20:16 +0700

    [Update]Can add room AND del room

--- src/components/formManageDepartment.js
+++ src/components/formManageDepartment.js
@@ -9,33 +9,173 @@ const departmentOption = [
     { key: 2, text: 'Lab', value: 'Lab' },
 ]
 
+const showContentForm = props => {
+    let tmp = ''
+    if (props.activeItem === 'department') {
+        tmp = (<div>
+            <Segment attached='bottom' color='teal' >
+                <Header>Add Department</Header>
+                <Form>
+                    {/* <Form.Group widths='equal'> */}
+                    <Form.Field
+                        required
+                        control={Input}
+                        value={props.departmentName}
+                        label='Department Name'
+                        placeholder='Department Name'
+                        onChange={(e, { value }) => props.setField("departmentName", value)} />
+                    <Form.Field
+                        required
+                        control={Select}
+                        value={props.typeOfDepartment}
+                        label='Choose Department Or Lab type'
+                        options={departmentOption}
+                        placeholder='Choose Department Or Lab type'
+                        onChange={(e, { value }) => props.setField("typeOfDepartment", value)} />
+                    {/* </Form.Group> */}
+                </Form>
+                <br />
+                <center>
+                    <Button
+                        color='blue'
+                        onClick={() => {
+                            props.addDepartment();
+                        }}> Add
+                </Button>
+                </center>
+            </Segment>
+        </div>
+        )
+    }
+    return tmp
+}
+
+const showContent = props => {
+    let tmp = "";
+    if (props.activeItem === 'department' || props.activeItem === 'rooms') {
+        tmp = (<div>
+            <Segment color='blue' style={{ maxHeight: '200%' }}>
+                <Header>List Of Department</Header>
+                <List animated verticalAlign='middle' divided relaxed='very' >
+                    {props.activeItem === 'department' ? props.showAllDepartment() : props.showAllRoom()}
+                </List>
+            </Segment>
+        </div>
+        );
+    }
+    return tmp;
+};
+
+const showFormRoom = props => {
+    let tmp = "";
+    if (props.activeItem === 'rooms') {
+        tmp = (<div>
+            <Segment attached='bottom' color='teal' >
+                <Header>Add Room</Header>
+                <Form>
+                    <Form.Group widths='equal'>
+                        <Form.Field
+                            required
+                            control={Input}
+                            value={props.roomNumber}
+                            label='Room Number'
+                            placeholder='Room Number'
+                            onChange={(e, { value }) => props.setField("roomNumber", value)} />
+                        <Form.Field
+                            required
+                            control={Input}
+                            value={props.building}
+                            label='Building'
+                            placeholder='Building'
+                            onChange={(e, { value }) => props.setField("building", value)} />
+                    </Form.Group>
+                    <Form.Group widths='equal'>
+                        <Form.Field
+                            required
+                            control={Input}
+                            value={props.floor}
+                            label='Floor'
+                            placeholder='Floor'
+                            onChange={(e, { value }) => props.setField("floor", value)} />
+                        <Form.Field
+                            required
+                            control={Select}
+                            value={props.departmentValueId}
+                            label='Choose Department'
+                            options={props.allDepartments}
+                            placeholder='Choose Department'
+                            onChange={(e, { value }) => props.setField("departmentValueId", value)} />
+                    </Form.Group>
+                </Form>
+                <br />
+                <center>
+                    <Button
+                        color='blue'
+                        onClick={() => {
+                            props.addRooms();
+                        }}> Add
+                </Button>
+                </center>
+            </Segment>
+        </div>
+        );
+    }
+    return tmp;
+};
+
 const formManageDepartment = props => {
     console.log("props", props);
 
     return (
         <div>
-            <Grid centered divided style={{ marginTop: "2.5%" }}>
+            <Grid centered style={{
+                marginTop: "2.5%",
+            }}>
                 <Header as='h2' color='teal'>Department Management</Header>
                 <Grid.Row stretched >
+                    <Grid.Column width={3} style={{ maxHeight: '60%', minHeight: '60%', height: 450 }}>
+                        <Menu pointing vertical>
+                            <Menu.Item
+                                name='Add or Delete Department'
+                                onClick={() => {
+                                    props.setField('activeItem', 'department');
+                                }}
+                                active={props.activeItem === 'department' ? true : false}
+                            >Department
+                            </Menu.Item>
+                            <Menu.Item
+                                name='Rooms'
+                                onClick={() => {
+                                    props.setField('activeItem', 'rooms');
+                                }}
+                                active={props.activeItem === 'rooms' ? true : false}
+                            >Rooms
+                            </Menu.Item>
+                            <Menu.Item
+                                name='Doctors'
+                                onClick={() => {
+                                    props.setField('activeItem', 'doctors');
+                                }}
+                                active={props.activeItem === 'doctors' ? true : false}
+                            >Doctors
+                            </Menu.Item>
+                        </Menu>
+                    </Grid.Column>
                     <Grid.Column width={6} style={{ maxHeight: '60%', minHeight: '60%', overflowY: 'scroll', }}>
-                        <Segment color='blue' style={{ maxHeight: '200%' }}>
+                        {showContent(props)}
+                        {/* <Segment color='blue' style={{ maxHeight: '200%' }}>
                             <Header>List Of Department</Header>
                             <List animated verticalAlign='middle' divided relaxed='very' >
-                                {/* <List.Item>
-                                        <Icon name='plus' />
-                                        <List.Content>
-                                            <List.Header>Helen</List.Header>
-                                        </List.Content>
-                                    </List.Item> */}
                                 {props.showAllDepartment()}
                             </List>
-                        </Segment>
+                        </Segment> */}
                     </Grid.Column>
                     <Grid.Column width={6} style={{ width: "50%", height: '60%' }}>
-                        <Segment attached='bottom' color='teal' >
+                        {showContentForm(props)}
+                        {showFormRoom(props)}
+                        {/* <Segment attached='bottom' color='teal' >
                             <Header>Add Department</Header>
                             <Form>
-                                {/* <Form.Group widths='equal'> */}
                                 <Form.Field
                                     required
                                     control={Input}
@@ -51,9 +191,7 @@ const formManageDepartment = props => {
                                     options={departmentOption}
                                     placeholder='Choose Department Or Lab type'
                                     onChange={(e, { value }) => props.setField("typeOfDepartment", value)} />
-                                {/* </Form.Group> */}
                             </Form>
-                            <br />
                             <br />
                             <center>
                                 <Button
@@ -63,7 +201,7 @@ const formManageDepartment = props => {
                                     }}> Add
                                 </Button>
                             </center>
-                        </Segment>
+                        </Segment> */}
                     </Grid.Column>
                 </Grid.Row>
             </Grid>
--- src/containners/addOrdeleteDepartment.js
+++ src/containners/addOrdeleteDepartment.js
@@ -16,22 +16,35 @@ class addOrdeleteDepartment extends Component {
         userType: '',
 
         listDepartment: [],
+        listRooms: [],
 
         departmentName: '',
         typeOfDepartment: '',
+
+        roomNumber: 0,
+        floor: 0,
+        building: '',
+        allDepartments: [{ key: "", text: "", value: "" }],
+        departmentValueId: 0,
+
+        activeItem: 'department',
     }
     componentWillMount = async () => {
         const { empId, departmentId, type } = JSON.parse(localStorage.getItem('userData'))
 
         const allDepartment = await axios.get(`/getAllDepartment`);
-        console.log(allDepartment)
+        const allRoom = await axios.get(`/getAllRoom`);
+
+        const departmentOption = this.dropdownDoctors(allDepartment);
 
         await this.getListDepartment()
         this.setState({
             nurseId: empId,
             departmentId,
             userType: type,
-            listDepartment: allDepartment.data
+            listDepartment: allDepartment.data,
+            listRooms: allRoom.data,
+            allDepartments: departmentOption
         })
         console.log(this.state.listDepartment)
     }
@@ -42,12 +55,27 @@ class addOrdeleteDepartment extends Component {
 
     getListDepartment = async () => {
         const allDepartment = await axios.get(`/getAllDepartment`);
-        console.log(allDepartment)
         await this.setState({
             listDepartment: allDepartment.data
         })
         console.log(this.state.listDepartment)
     }
+
+    getListRooms = async () => {
+        const allRoom = await axios.get(`/getAllRoom`);
+        await this.setState({
+            listRooms: allRoom.data
+        })
+    }
+
+    dropdownDoctors = allDepartment => {
+        const department = allDepartment.data.map(data => ({
+            key: data.departmentId,
+            text: data.department,
+            value: data.departmentId
+        }));
+        return department;
+    };
 
     showAllDepartment = (i) => {
         let tmp = ""
@@ -71,6 +99,28 @@ class addOrdeleteDepartment extends Component {
         return tmp
     }
 
+    showAllRoom = (i) => {
+        let tmp = ""
+        const datas = this.state.listRooms
+        tmp = datas.map((data, i) => (
+            < List.Item key={i}>
+                <List.Content floated='right'>
+                    <Button color='red' size='mini'
+                        onClick={() => {
+                            this.deleteRoom(i);
+                        }}> Delete
+                    </Button>
+                </List.Content>
+                <Icon name='building' color='blue' />
+                <List.Content>
+                    <List.Header>{data.roomId}</List.Header>
+                    <List.Header>{data.department}</List.Header>
+                </List.Content>
+            </List.Item >
+        ))
+        return tmp
+    }
+
     addDepartment = async () => {
         //addDepartment
         await axios.post("/addDepartment", {
@@ -86,9 +136,27 @@ class addOrdeleteDepartment extends Component {
         console.log('suc')
     }
 
+    addRooms = async () => {
+        //addDepartment
+        await axios.post("/addRoom", {
+            roomId: this.state.roomNumber,
+            floor: this.state.floor,
+            departmentId: this.state.departmentValueId,
+            building: this.state.building,
+        })
+        swal("Success!", `Add Successful`, "success");
+        this.setState({
+            roomId: '',
+            floor: '',
+            departmentId: '',
+            building: ''
+        })
+        await this.getListRooms()
+        console.log('suc')
+    }
+
     deleteDepartment = async (i) => {
         const departmentId = this.state.listDepartment[i].departmentId
-        console.log(departmentId)
         let swl = ''
         swl = swal({
             title: "Are you sure?",
@@ -100,16 +168,45 @@ class addOrdeleteDepartment extends Component {
                 if (willDelete) {
                     await axios.delete(`/deleteDepartment/${departmentId}`)
                     await this.getListDepartment()
+                    swal("Poof! Your Department has been deleted!", {
+                        icon: "success",
+                    });
                 }
-                swal("Poof! Your Department has been deleted!", {
-                    icon: "success",
-                });
+                
                 console.log('succ del')
             });
     }
 
+    deleteRoom = async (i) => {
+        const roomId = this.state.listRooms[i].roomId
+        console.log(roomId)
+        let swl = ''
+        swl = swal({
+            title: "Are you sure?",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+        })
+            .then(async (willDelete) => {
+                if (willDelete) {
+                    await axios.delete(`/deleteRoom/${roomId}`)
+                    await this.getListRooms()
+                    swal("Poof! Your Department has been deleted!", {
+                        icon: "success",
+                    });
+                }
+
+                console.log('succ del')
+            });
+
+    }
+
+    setField = (field, value) => {
+        this.setState({ [field]: value });
+    };
+
     render() {
-        console.log(this.state.departmentName, this.state.typeOfDepartment)
+        console.log(this.state.departmentValueId)
         return (
             <div >
                 <Headerbar />
@@ -118,11 +215,21 @@ class addOrdeleteDepartment extends Component {
                     //state
                     departmentName={this.state.departmentName}
                     typeOfDepartment={this.state.typeOfDepartment}
+                    activeItem={this.state.activeItem}
+                    roomNumber={this.state.roomNumber}
+                    floor={this.state.floor}
+                    building={this.state.building}
+                    allDepartment={this.state.allDepartment}
+                    departmentValueId={this.state.departmentValueId}
+                    allDepartments={this.state.allDepartments}
 
                     //method
                     setField={this.setField}
                     addDepartment={this.addDepartment}
+                    addRooms={this.addRooms}
                     showAllDepartment={this.showAllDepartment}
+                    showAllRoom={this.showAllRoom}
+
                 />
             </div>
         );
